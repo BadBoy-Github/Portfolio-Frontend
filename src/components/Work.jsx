cf0a0e8a2a6c3fa108ec349046237ba3346353eb
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-08 14:03:43 +0530

    works

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,39 +4,43 @@
 import ProjectCard from './ProjectCard';
 
 const works = [
-
-    {
-        imgSrc: '/images/project-5.jpg',
-        title: 'Stellar AI Chatbot',
-        tags: ['AI Model', 'Chat'],
-        projectLink: 'https://stellar-steel-tau.vercel.app/'
-    },
-    {
-        imgSrc: '/images/project-4.jpg',
-        title: 'Spotify Clone',
-        tags: ['Music', 'Clerk', 'Clone'],
-        projectLink: 'https://spotify-clone-6rda.onrender.com/'
-    },
-    {
-        imgSrc: '/images/project-3.jpg',
-        title: 'eCommerce website',
-        tags: ['eCommerce', 'Stripe'],
-        projectLink: 'https://full-stack-ecommerce-h47u.vercel.app/'
-    },
-    {
-        imgSrc: '/images/project-2.jpg',
-        title: 'Iron vs Cancer',
-        tags: ['FeNPs', 'Encapsule', 'Cancer'],
-        projectLink: '/error'
-    },
-    {
-        imgSrc: '/images/project-1.jpg',
-        title: 'Silver vs Bacteria',
-        tags: ['AgNPs', 'Antimicrobial'],
-        projectLink: 'https://journals.innovareacademics.in/index.php/ijap/article/view/51711'
-    },
-    
-
+  {
+    imgSrc: "/images/project-5.jpg",
+    title: "Stellar AI Chatbot",
+    tags: ["AI Model", "Chat"],
+    projectLink: "https://stellar-steel-tau.vercel.app/",
+  },
+  {
+    imgSrc: "/images/project-4.jpg",
+    title: "Spotify Clone",
+    tags: ["Music", "Clerk", "Clone"],
+    projectLink: "https://spotify-clone-6rda.onrender.com/",
+  },
+  {
+    imgSrc: "/images/project-3.jpg",
+    title: "eCommerce website",
+    tags: ["eCommerce", "Stripe"],
+    projectLink: "https://full-stack-ecommerce-h47u.vercel.app/",
+  },
+  {
+    imgSrc: "/images/project-6.jpg",
+    title: "ToDo App",
+    tags: ["Todo", "CRUD"],
+    projectLink: "https://to-do-app-se8u.onrender.com/",
+  },
+  {
+    imgSrc: "/images/project-2.jpg",
+    title: "Iron vs Cancer",
+    tags: ["FeNPs", "Encapsule", "Cancer"],
+    projectLink: "/error",
+  },
+  {
+    imgSrc: "/images/project-1.jpg",
+    title: "Silver vs Bacteria",
+    tags: ["AgNPs", "Antimicrobial"],
+    projectLink:
+      "https://journals.innovareacademics.in/index.php/ijap/article/view/51711",
+  },
 ];
 
 const Work = () => {
