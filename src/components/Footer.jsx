3149b802e252d47230e60b24f24f60331ca857fe
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-22 20:38:39 +0530

    footer name change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,86 +51,74 @@ const socials = [
 
 const Footer = () => {
     return (
-        <footer className="section">
-            <div className="container">
+      <footer className="section">
+        <div className="container">
+          <div className="lg:grid lg:grid-cols-2">
+            <div className="mb-10">
+              <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
+                Let&apos;s Collab today!
+              </h2>
 
-                <div className="lg:grid lg:grid-cols-2">
-
-                    <div className="mb-10">
-                        <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
-                            Let&apos;s Collab today!
-                        </h2>
-
-                        <ButtonPrimary
-                            href="https://www.linkedin.com/in/elayabarathi/"
-                            label="Start Collab"
-                            icon="chevron_right"
-                            classes="reveal-up"
-                            target="_blank"
-                        />
-                    </div>
-
-                    <div className="grid grid-cols-2 gap-4 lg:pl-20">
-
-                        <div>
-                            <p className="mb-2 reveal-up">Sitemap</p>
-
-                            <ul>
-                                {sitemap.map(({ label, href }, key) => (
-                                    <li key={key}>
-                                        <a
-                                            href={href}
-                                            className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
-                                        >
-                                            {label}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-
-                        <div>
-                            <p className="mb-2 reveal-up">Socials</p>
-
-                            <ul>
-                                {socials.map(({ label, href }, key) => (
-                                    <li key={key}>
-                                        <a
-                                            href={href}
-                                            target="_blank"
-                                            className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
-                                        >
-                                            {label}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="flex items-center justify-between pt-10 mb-8">
-                    <a
-                        href="/"
-                        className="logo reveal-up"
-                    >
-                        <img
-                            src="/images/logo.svg"
-                            width={40}
-                            height={40}
-                            alt="Logo"
-                        />
-                    </a>
-
-                    <p className="text-zinc-500 text-sm reveal-up">
-                        &copy; 2025 <span className="text-zinc-200"> Coding with Panda</span>
-                    </p>
-                </div>
-
+              <ButtonPrimary
+                href="https://www.linkedin.com/in/elayabarathi/"
+                label="Start Collab"
+                icon="chevron_right"
+                classes="reveal-up"
+                target="_blank"
+              />
             </div>
-        </footer>
-    )
+
+            <div className="grid grid-cols-2 gap-4 lg:pl-20">
+              <div>
+                <p className="mb-2 reveal-up">Sitemap</p>
+
+                <ul>
+                  {sitemap.map(({ label, href }, key) => (
+                    <li key={key}>
+                      <a
+                        href={href}
+                        className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
+                      >
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <p className="mb-2 reveal-up">Socials</p>
+
+                <ul>
+                  {socials.map(({ label, href }, key) => (
+                    <li key={key}>
+                      <a
+                        href={href}
+                        target="_blank"
+                        className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
+                      >
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between pt-10 mb-8">
+            <a href="/" className="logo reveal-up">
+              <img src="/images/logo.svg" width={40} height={40} alt="Logo" />
+            </a>
+
+            <p className="text-zinc-500 text-sm reveal-up">
+              &copy; 2025{" "}
+              <span className="text-zinc-200">· Elayabarathi M V</span>
+            </p>
+          </div>
+        </div>
+      </footer>
+    );
 }
 
 export default Footer
