805cbfb25b5bc908db5df9f926f8413093c14889
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-07-25 11:03:49 +0530

    education component title

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,8 +2,12 @@
 
 const Education = () => {
   return (
-    <div>Education</div>
-  )
+    <section id="education" className="section">
+      <div className="px-4 mx-auto lg:px-6 xl:max-w-6xl container relative">
+        <h2 className="headline-2 reveal-up">Education</h2>
+      </div>
+    </section>
+  );
 }
 
 export default Education
