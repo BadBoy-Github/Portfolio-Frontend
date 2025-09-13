be3a712514018c46757b7b43d56c818ea59e18bb
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-09-13 18:36:44 +0530

    chat sample ui design added -> not yet completed;

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,46 +1,90 @@
 
 
+// React icons
+import { RiRobot2Fill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
 
 const Chat = () => {
   return (
     <>
       <section id="chat" className="section ">
         <div className="px-4 mx-auto lg:px-6 xl:max-w-6xl container relative">
-          <h2 className="headline-2">ChatBot</h2>
+          <h2 className="headline-2">Talk With My Portfolio</h2>
           <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
-            Chatbot description
+            Explore my portfolio, skills, and resume in a conversational way
           </p>
         </div>
-        <div className="container">
+        <div className="px-4 mx-auto lg:px-6 xl:max-w-6xl container">
           <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 shadow-xl grid grid-cols-1 lg:grid-cols-[40%_60%] gap-6 lg:gap-4">
             <div className="">
-              <h1>Chatbot</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
-                laudantium adipisci harum distinctio quidem et quam similique
-                nulla in nesciunt, sunt ratione nam porro repudiandae magni.
-                Voluptatibus at totam deleniti cumque, iste ducimus quaerat
-                error maxime distinctio eos quisquam amet voluptatum aspernatur
-                earum suscipit beatae labore placeat laboriosam provident
-                voluptatem!
+              <h1 className="text-xl font-semibold text-sky-400">
+                About the Chatbot
+              </h1>
+              <p className="mt-3 text-zinc-300 leading-relaxed">
+                This chatbot is designed to make exploring my portfolio more
+                interactive. You can ask questions about my{" "}
+                <span className="text-sky-400">skills</span>,
+                <span className="text-sky-400"> projects</span>, or even my
+                <span className="text-sky-400"> resume</span>, and it will guide
+                you to the right information. Instead of scrolling through
+                pages, just have a conversation and quickly discover what
+                you&apos;re looking for. Think of it as a friendly assistant
+                built right into my portfolio.
               </p>
-              <button className="bg-gray-500 px-2 py-1 rounded-lg text-sm">Download Resume</button>
+              <button className="bg-sky-600 hover:bg-sky-400 text-zinc-800 px-3 py-2 rounded-lg font-semibold text-sm flex items-center justify-center gap-2 mt-4">
+                Download Resume
+                <span
+                  className="material-symbols-rounded size-3 "
+                  aria-hidden="true"
+                >
+                  download
+                </span>
+              </button>
             </div>
-            <div className="bg-zinc-700/70 w-full h-[400px] rounded-2xl flex flex-col justify-between gap-2">
+            <div className="bg-zinc-800/70 w-full h-[400px] rounded-2xl flex flex-col justify-between gap-2 hover:bg-zinc-700/30 transition-all duration-500 hover:ring-1 hover:ring-zinc-500/10 hover:ring-inset">
               <div className=" h-full rounded-2xl p-4">
-                <div className=" h-[308px] overflow-y-scroll scrollbar-thin">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit quia voluptatum, incidunt officia nesciunt dicta delectus ea vel consequuntur ipsum cum facere perferendis ad praesentium natus repellendus iste recusandae qui iusto dolorem odit. Modi veniam reprehenderit rem culpa, enim laudantium accusantium iste neque obcaecati excepturi itaque repellendus tempora, facere sint, laborum explicabo optio sit. Alias vitae fugit eveniet deserunt nobis esse enim atque commodi, a cumque aut amet nesciunt rem id repudiandae corporis laboriosam consectetur eos facere delectus earum sit natus ipsa. Voluptatum, hic? Magni eius, temporibus distinctio consectetur alias laborum nobis? Perspiciatis optio laborum eveniet sequi omnis quisquam laboriosam ducimus, numquam, molestias dolorem doloremque recusandae sed facilis illo quae voluptatibus repellendus atque ullam pariatur culpa fugit, possimus minus aliquid? Illum commodi impedit suscipit vitae amet maiores est veniam nulla repellendus illo corporis fugiat delectus neque iusto accusantium saepe reiciendis, alias nobis error cupiditate eius quasi rerum? Officia ratione voluptatem autem voluptatum porro nemo consectetur ipsam aut laborum vitae quis ut repellat quasi, perspiciatis id magnam? Pariatur dolorum cumque quae earum fugit dicta totam magnam labore? Recusandae aperiam rem eius in hic doloribus necessitatibus sint! Repellendus molestiae cupiditate atque cumque quidem impedit deleniti incidunt odio iure fugit, aspernatur consectetur neque id distinctio hic! Tempora ex necessitatibus enim obcaecati aut quis perferendis fugit sapiente sunt voluptates, fugiat fuga impedit eligendi cupiditate quisquam commodi saepe iusto autem! Voluptatum explicabo consequuntur quia laboriosam eveniet praesentium dicta atque beatae, est incidunt, aperiam nesciunt. Hic harum iusto perspiciatis molestiae labore, ipsam voluptatem inventore enim illum natus, molestias quod! Ipsam tenetur sit amet ut, ipsa quae magnam nesciunt magni error beatae. Ipsam architecto corrupti voluptatem ea iure quod vitae cumque accusantium sed voluptate quisquam obcaecati provident perspiciatis odio inventore, magni incidunt dolor assumenda consequatur animi deserunt optio. Accusantium iste ducimus nemo facere vel sed? Praesentium error reprehenderit aut nisi modi. Cumque, dolore? Delectus voluptas fugiat esse accusantium, aperiam odit natus sit aut corrupti alias quae saepe sequi, voluptate soluta! Ipsum cupiditate adipisci consectetur laboriosam sequi atque dicta repudiandae placeat nobis? Temporibus, distinctio natus! Earum alias repellat animi quas consequuntur necessitatibus architecto placeat quae officiis distinctio, quasi ea similique dolores esse laborum aliquid tempora eaque quis nam aperiam quaerat nemo vero consectetur. Esse nam eum ipsa qui sunt libero molestias delectus ducimus nemo quod sint, repellendus adipisci quidem commodi praesentium quisquam. Accusamus atque recusandae iste pariatur perferendis. Dicta provident tempora reprehenderit voluptates eveniet itaque dolor libero harum iure necessitatibus nemo omnis debitis rerum laudantium velit, ab nulla voluptatibus ut autem eum deserunt? Repellendus, autem? Omnis ab eveniet dolores est repellendus corporis, provident sunt natus? Et ut accusantium quo porro quasi maiores facilis explicabo dicta voluptates minus! Sint eos nam necessitatibus debitis excepturi, unde at distinctio illum assumenda tempora ratione iure incidunt voluptas nobis autem adipisci illo ipsum, placeat repudiandae. Aut quas officia possimus suscipit totam culpa. Consequuntur, ut. Saepe quasi commodi deleniti molestias ex cum maxime animi non autem sapiente, consequuntur dicta quisquam, culpa quis! Animi necessitatibus eaque delectus ipsam praesentium natus iure corrupti asperiores doloremque quisquam alias iusto omnis, maxime quidem commodi nemo itaque eos nam quae quibusdam recusandae odio debitis voluptatibus. Earum eius tempora ut rerum, nobis ab dolorum quod impedit ipsum ratione, debitis tenetur officiis cumque cum doloremque sequi facere, at nesciunt. Amet deserunt fugit, quos, nisi illo cumque aspernatur molestiae, sequi incidunt iste culpa voluptate molestias? Sint quisquam nulla suscipit excepturi iure dolorum amet quas a ipsa perferendis tempora illum assumenda accusamus, pariatur optio error delectus aperiam animi ab corrupti doloribus. Eos hic nisi harum exercitationem accusantium odio quo illum, neque nesciunt in suscipit fugiat voluptates aliquid molestias magni dicta at illo architecto? Et ipsum laborum doloribus ullam magni optio, libero reprehenderit unde quaerat expedita, tenetur consectetur perspiciatis amet nostrum a, fugit cumque ipsa explicabo. Vel molestias expedita corporis velit! Minima, animi vero. Fugit nam consequatur quam eligendi earum. Odit quos magni inventore blanditiis impedit nulla quaerat unde eaque velit saepe animi voluptates, commodi error? Animi ad aperiam quia tenetur modi nesciunt vel dicta minima unde distinctio error quidem dolorum officia, provident nobis perferendis doloremque vitae? Porro suscipit iusto aperiam voluptatibus dolor magni harum earum? A dolores reiciendis magnam perferendis quasi cum suscipit temporibus dolorem doloremque vero vel dolorum, sed iure obcaecati dolor in maiores ducimus facere doloribus! Molestiae, labore et. Dolores eos fugit provident, a voluptatum similique eaque reprehenderit est sint ducimus corporis obcaecati accusamus distinctio alias, suscipit, vitae ipsam aperiam consectetur quisquam necessitatibus voluptates quas. Nobis tempore magni repellendus autem adipisci facere quas numquam explicabo facilis, voluptates reiciendis sapiente nam, laboriosam ullam sed quo! Voluptatum incidunt corrupti id. Delectus labore ipsa quidem laborum, esse aut, consequatur doloremque reprehenderit alias aperiam a itaque adipisci sit! Optio tempore veniam voluptate beatae, aliquid maxime? Nostrum, accusantium. Cumque, nobis! Sed fugiat accusamus cum non? Rem incidunt labore delectus sunt asperiores eum perferendis minima reprehenderit doloremque in eos, ducimus aliquam odit? Possimus cupiditate vel nostrum facilis ipsum nulla delectus explicabo, illum omnis magnam earum. Est corporis exercitationem delectus eveniet deserunt? Inventore unde vel consequuntur! Beatae velit delectus dicta non nesciunt magni ipsa obcaecati odio, placeat, mollitia officia nihil dignissimos repellat cupiditate! Quia vero provident saepe aut animi, consectetur, reiciendis expedita odit temporibus asperiores atque minima amet nostrum deserunt aliquid? In aspernatur aut perspiciatis perferendis mollitia obcaecati modi corrupti! Mollitia aliquid eveniet, at tenetur iure ducimus doloribus repellendus ullam sapiente! Quo similique nemo, atque voluptates accusantium perspiciatis amet harum illo nulla fugiat cumque voluptatibus ex corrupti quaerat enim nesciunt neque ratione earum. Delectus vel doloribus distinctio tempora voluptatibus provident fugit earum animi voluptate ducimus aperiam nesciunt corporis ipsam cupiditate illo nam ex, incidunt, ad, voluptatum id! Et perferendis maiores fugit debitis dolores. Odit quod deserunt at voluptatibus illum, neque maiores ea molestiae velit doloremque optio! Sint minus repellendus ea, a magni quae. Possimus et quia tenetur quo obcaecati molestias nulla distinctio, ab eius incidunt necessitatibus! Quae quas harum voluptatibus unde recusandae alias, libero quo inventore quidem quaerat eveniet veniam sint, porro repellendus maiores expedita? Nobis consequatur eum magni, nulla quidem molestias, perferendis ullam ad dignissimos iure dicta!
+                <div className=" h-[300px] overflow-y-scroll scrollbar-thin flex flex-col gap-4 px-2">
+                  <div className="flex items-center gap-3 mt-2">
+                    <div className="flex items-center justify-center bg-sky-700 p-2 rounded-full">
+                      <RiRobot2Fill className="size-5" />
+                    </div>
+                    <p className="bg-sky-600/20 ring-1 ring-sky-700/40 px-2 py-1 rounded-lg text-zinc-200">
+                      Hi, I&apos;m a friendly Chatbot that lets you interact
+                      with the portfolio and CV. How can I help you?
+                    </p>
+                  </div>
+                  <div className="flex flex-row-reverse items-center gap-3">
+                    <div className="flex items-center justify-center bg-emerald-700 p-2 rounded-full">
+                      <FaUser className="size-5" />
+                    </div>
+                    <p className="bg-emerald-600/20 ring-1 ring-emerald-700/40 px-2 py-1 rounded-lg text-zinc-200">
+                      What&apos;s the latest project you have done recently?
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-3 mt-2">
+                    <div className="flex items-center justify-center bg-sky-700 p-2 rounded-full">
+                      <RiRobot2Fill className="size-5" />
+                    </div>
+                    <p className="bg-sky-600/20 ring-1 ring-sky-700/40 px-2 py-1 rounded-lg text-zinc-200">
+                      ···
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className=" px-4 pt-1 pb-4 gap-3 rounded-2xl grid grid-cols-[84%_14%]">
+              <div className=" px-4 pt-1 pb-4 gap-3 rounded-2xl grid grid-cols-[92%_6%]">
                 <input
                   type="text"
-                  className="bg-zinc-800 text-sky-100 outline-none rounded-lg px-2 py-1 transition-all duration-500"
-                  placeholder="Wanna know about me?"
+                  className="bg-zinc-800 text-sky-100 outline-none outline-zinc-500 hover:outline-sky-700 active:outline-sky-700 rounded-lg px-3 py-2 transition-all duration-500 placeholder:text-sm text-sm"
+                  placeholder="Hey there, what skills are you best at?"
                   size={40}
+                  tabIndex={0}
                 />
-                <button className="bg-zinc-800 px-2 py-1 rounded-lg text-sm">
-                  Send
+                <button
+                  className="bg-zinc-800 text-zinc-200 hover:text-zinc-800  hover:bg-sky-600  px-2 py-1 outline-none outline-zinc-500 hover:outline-sky-700 rounded-lg text-sm transition-all duration-300 flex items-center justify-center"
+                  type="submit"
+                >
+                  <IoSend className="size-4" />
                 </button>
               </div>
             </div>
