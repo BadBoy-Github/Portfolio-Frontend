a6b12065ace3061fcf40a520666615fa18b58091
BadBoy-Github <elayabarathiedison@gmail.com>
2025-10-21 16:00:59 +0530

    changes the speed of animation in the navbar, now it looks fine;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,30 +9,41 @@ const Header = () => {
     const [navOpen, setNavOpen] = useState(false);
 
     return (
-        <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]" >
-                <h1>
-                    <a href="/" className="logo">
-                        <img src="/img/icons/favicon.svg" alt="Elayabarathi" width={40} height={40} />
-                    </a>
-                </h1>
+      <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+        <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+          <h1>
+            <a href="/" className="logo">
+              <img
+                src="/img/icons/favicon.svg"
+                alt="Elayabarathi"
+                width={40}
+                height={40}
+              />
+            </a>
+          </h1>
 
-                <div className="relative md:justify-self-center">
-                    <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
-                        <span className="material-symbols-rounded">
-                            {navOpen ? 'close' : 'menu'}
-                        </span>
-                    </button>
+          <div className="relative md:justify-self-center">
+            <button
+              className="menu-btn md:hidden"
+              onClick={() => setNavOpen((prev) => !prev)}
+            >
+              <span className="material-symbols-rounded">
+                {navOpen ? "close" : "menu"}
+              </span>
+            </button>
 
-                    <Navbar navOpen={navOpen} />
-                </div>
+            <Navbar navOpen={navOpen} />
+          </div>
 
-                <a href="#contactme" className="btn btn-secondary max-lg:hidden lg:justify-self-end">
-                    Contact Me
-                </a>
-            </div>
-        </header>
-    )
+          <a
+            href="#contactme"
+            className="max-w-max h-10 flex justify-center items-center gap-2 px-4 rounded-xl font-medium text-xs md:text-sm ring-1 ring-zinc-50/5 ring-inset transition-[background-color] bg-zinc-50 text-zinc-900 active:bg-zinc-50/80 max-lg:hidden lg:justify-self-end"
+          >
+            Contact Me
+          </a>
+        </div>
+      </header>
+    );
 }
 
 export default Header
