fe10818a45dc817792bb39661f00b2f41a604526
BadBoy-Github <elayabarathiedison@gmail.com>
2026-03-25 16:23:23 +0530

    certificate, education, experience, review, skill data images updated to cloud

--- src/data/ReviewData.js
+++ src/data/ReviewData.js
@@ -5,35 +5,35 @@ const reviews = [
         content:
             "Skilled coder with impressive projects like CRUD app, eCommerce website, and Spotify clone. His technical prowess and creativity shine through.",
         name: "Sanjay S",
-        imgSrc: "/img/reviews/people-2.webp",
+        imgSrc: "https://res.cloudinary.com/dz53e3szr/image/upload/v1774435439/people-2_wfqkwm.webp",
         company: "Junior Software Developer",
     },
     {
         content:
             "Outstanding individual with diverse extracurricular activities, always eager to explore and learn. His enthusiasm and versatility are remarkable.",
         name: "Subaash Sivan N",
-        imgSrc: "/img/reviews/people-3.webp",
+        imgSrc: "https://res.cloudinary.com/dz53e3szr/image/upload/v1774435441/people-3_l5fhti.webp",
         company: "Associate Bank Manager",
     },
     {
         content:
             "Talented beginner web developer who created a basic eCommerce website. His commitment to learning and improving their skills is evident.",
         name: "Dhil Rohith B",
-        imgSrc: "/img/reviews/people-4.webp",
+        imgSrc: "https://res.cloudinary.com/dz53e3szr/image/upload/v1774435442/people-4_msu4pm.webp",
         company: "Senior Backend Developer",
     },
     {
         content:
             "Exceptional biotechnologist who completed his B.Tech in Biotechnology with flying colors, expertise and dedication are truly commendable.",
         name: "Kishore G",
-        imgSrc: "/img/reviews/people-6.webp",
+        imgSrc: "https://res.cloudinary.com/dz53e3szr/image/upload/v1774435438/people-5_nbykxu.webp",
         company: "Freelancer",
     },
     {
         content:
             "A good learner who learns from mistakes and excels in problem-solving. His ability to adapt and overcome challenges is impressive.",
         name: "Asothama Chakravarthi V",
-        imgSrc: "/img/reviews/people-1.webp",
+        imgSrc: "https://res.cloudinary.com/dz53e3szr/image/upload/v1774435439/people-1_fg2ola.webp",
         company: "Textile Business",
     },
 ];
