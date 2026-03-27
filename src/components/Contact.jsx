3706b5e046d42def1a5ee93ad2a0a4f61236b7a6
BadBoy-Github <elayabarathiedison@gmail.com>
2026-03-27 18:38:03 +0530

    PageSpeed analysis updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -222,11 +222,12 @@ const Contact = () => {
           </p>
 
           <div className="flex items-center gap-2 mt-auto">
-            {socialLinks.map(({ href, icon }, key) => (
+            {socialLinks.map(({ href, icon, alt }, key) => (
               <a
                 key={key}
                 href={href}
                 target="_blank"
+                aria-label={`Visit my ${alt} profile`}
                 className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color]
                                 hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 duration-500 text-2xl"
               >
